89229ea76417a4f044e86a07d24337e4cf2cf5f0
Deekshitha <deekshithanallamothu18@gmail.com>
2026-08-15 21:21:36 +0530

    Practiced funvtions

---
+++ functions.py
@@ -0,0 +1,16 @@
+# def greet(name):
+#     print("Hello", name)
+
+# greet("Deekshitha")
+
+# def add(a,b):
+#     return a+b
+# print(add(3,4))
+
+def check_even(n):
+    if n%2==0:
+        return "Even"
+    else:
+        return "Odd"
+
+print(check_even(5))
