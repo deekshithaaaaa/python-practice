dd14101e646f2ac274518587a3bf84dd08b950a8
Deekshitha <deekshithanallamothu18@gmail.com>
2026-08-17 21:32:23 +0530

    funtions

--- functions.py
+++ functions.py
@@ -1,11 +1,11 @@
-# def greet(name):
-#     print("Hello", name)
+def greet(name):
+    print("Hello", name)
 
-# greet("Deekshitha")
+greet("Deekshitha")
 
-# def add(a,b):
-#     return a+b
-# print(add(3,4))
+def add(a,b):
+    return a+b
+print(add(3,4))
 
 def check_even(n):
     if n%2==0:
@@ -14,3 +14,12 @@ def check_even(n):
         return "Odd"
 
 print(check_even(5))
+
+l=[1,2,3,4,5]
+p=map(lambda x:x**2,l)
+print(list(p))
+
+l=[1,2,3,4,5]
+p=filter(lambda x:x%2==0,l)
+print(list(p))
+
