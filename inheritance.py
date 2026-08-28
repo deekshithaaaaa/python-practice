6b5923932614ecf8d3445039b5b9ed7e06c96704
Deekshitha <deekshithanallamothu18@gmail.com>
2026-08-28 14:55:00 +0530

    Inheritance

--- inheritance.py
+++ inheritance.py
@@ -39,3 +39,83 @@ class Dog(Animal):
         print ("Dog barks")
 d1=Dog()
 print(d1.sound())
+
+class Person:
+    def __init__(self,name,age,city):
+        self.name=name
+        self.age=age
+        self.city=city
+class Student(Person):
+    def __init__(self,name,age,city,course):
+        super().__init__(name,age,city)
+        self.course=course
+s1=Student("Dee",21,"Hyd","Python")
+print(s1.name)
+print(s1.age)
+print(s1.city)
+print(s1.course)
+
+class Person:
+    def walk(self):
+        return "Person is walking"
+class Employee(Person):
+    def work(self):
+        return "Employee is working"
+class Manager(Employee):
+    def code(self):
+        return "Manager is coding"
+m1=Manager()
+print(m1.walk())
+print(m1.work())
+print(m1.code())
+
+class Employee:
+    def work(self):
+        return "Employee is working"
+class Developer(Employee):
+    def code(self):
+        return "Developer is coding"
+class Tester(Employee):
+    def test(self):
+        return "Tester is testing"
+d1 = Developer()
+t1 = Tester()
+print(d1.work())
+print(d1.code())
+print(t1.work())
+print(t1.test())
+
+        
+class Printer:
+    def Pmethod(self):
+        return "This is printer method"
+class Scanner:
+    def Smethod(self):
+        return "This is scanner method"
+class Machine(Printer, Scanner):
+    def Mmethod(self):
+        return "This is machine method"
+obj = Machine()
+print(obj.Pmethod())
+print(obj.Smethod())
+print(obj.Mmethod())
+
+
+
+class Person:
+    def Pmethod(self):
+        return "This is person method"
+class Student(Person):
+    def Smethod(self):
+        return "This is student method"
+class Employee(Person):
+    def Emethod(self):
+        return "This is employee method"
+class Intern(Student, Employee):
+    def Imethod(self):
+        return "This is intern method"
+obj = Intern()
+print(obj.Pmethod())
+print(obj.Smethod())
+print(obj.Emethod())
+print(obj.Imethod())
